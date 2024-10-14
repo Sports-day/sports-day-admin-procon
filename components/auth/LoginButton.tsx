2c793fe80759fbca334cb69d2e0de70119c8a4f5
testusuke <testusuke@gmail.com>
2024-10-15 00:06:42 +0900

    change: デモ用に認証無しで利用できるように変更

--- components/auth/LoginButton.tsx
+++ components/auth/LoginButton.tsx
@@ -1,51 +1,36 @@
 'use client'
 import {Button, Typography} from "@mui/material";
-import crypto from 'crypto';
-import * as querystring from "querystring";
-import {useEffect, useState} from "react";
+import * as React from "react";
 import {useTheme} from "@mui/material/styles";
+import {useRouter} from "next/navigation";
 
 export default function LoginButton() {
-    const [authorizationUrl, setAuthorizationUrl] = useState<string>('')
     const theme = useTheme()
+    const subDirectory = process.env.SUB_DIRECTORY ? process.env.SUB_DIRECTORY : "/"
+    const router = useRouter()
 
-    useEffect(() => {
-        const authorizationBaseUrl = process.env.NEXT_PUBLIC_OIDC_AUTHORIZE_URL
-        //  query params
-        const clientId = process.env.NEXT_PUBLIC_OIDC_CLIENT_ID
-        const redirectUri = process.env.NEXT_PUBLIC_OIDC_REDIRECT_URL
-        const scope = process.env.NEXT_PUBLIC_OIDC_SCOPE ?? "openid profile email"
-        //  generate random nonce and state
-        const nonce = crypto.randomBytes(16).toString('hex')
-
-        const queryData = {
-            "client_id": clientId,
-            "redirect_uri": redirectUri,
-            "response_type": "code",
-            "response_mode": "form_post",
-            "scope": scope,
-            "nonce": nonce,
-        }
-
-        //  make query string
-        const searchParams = querystring.stringify(queryData);
-        //  make url
-        setAuthorizationUrl(`${authorizationBaseUrl}?${searchParams}`)
-    }, [])
-
-    //  display name
-    const buttonDisplayName = process.env.NEXT_PUBLIC_OIDC_DISPLAY_NAME ?? "ログインできません"
+    const handleLogin = async () => {
+        await fetch("/api/auth/callback", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({})
+        })
+        router.replace(subDirectory)
+    }
 
     return (
         <Button
             variant="contained"
             color="primary"
-            href={authorizationUrl}
-            sx={{px:3, py:1.5, width:"100%", backgroundColor:`${theme.palette.text.primary}`}}
+            onClick={handleLogin}
+
+            sx={{px: 3, py: 1.5, width: "100%", backgroundColor: `${theme.palette.text.primary}`}}
             disableElevation
         >
             <Typography fontSize={"14px"} color={theme.palette.background.paper}>
-                {buttonDisplayName}
+                ログイン
             </Typography>
         </Button>
     );
